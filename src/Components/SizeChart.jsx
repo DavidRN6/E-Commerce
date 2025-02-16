5d9a9dcce5abfe8bb3a0b394d3d4c563ea72794b
David <yugiohgxd600@gmail.com>
2025-02-15 18:03:08 -0800

    edit size chart

--- src/Components/SizeChart.jsx
+++ src/Components/SizeChart.jsx
@@ -6,19 +6,19 @@ function SizeChart() {
 
 
   return (
-    <div className="w-96 flex flex-col gap-4">
+    <div className="flex flex-col gap-4">
       <div onClick={
         () => setShowSizeChart(!showSizeChart)
       } className="flex items-center gap-4 cursor-pointer w-44 transition">
         <h1 className="text-2xl">Size Chart</h1>
         <MdKeyboardArrowRight className="text-xl items-center" />
       </div>
-      <div className={`overflow-hidden transition-all duration-500 ${showSizeChart ? "h-[21rem]" : "h-0"}`}>
+      <div className={`overflow-hidden flex transition-all duration-500 ${showSizeChart ? "h-[21rem]" : "h-0"}`}>
         <img
           src="../size.png"
           alt="Size Chart"
           id="size-chart"
-          className="w-80 sm:w-80 flex flex-col mx-auto"
+          className="w-72 sm:w-80 sm:w-80 flex flex-col ml-4"
         />
       </div>
     </div>
